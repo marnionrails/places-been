c5b8707905152445fafd3cac6bea514ed39c96b4
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-07-07 21:51:19 -0400

    capture user input in variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 function Destination(location, timeOfYear, notes) {
-  this.location = location;
-  this.landmarks = [];
-  this.timeOfYear = timeOfYear;
-  this.notes = notes;
+    this.location = location;
+    this.landmarks = [];
+    this.timeOfYear = timeOfYear;
+    this.notes = notes;
 }
 
 let place1 = new Destination('Location', 1990, "notes");
 
-Destination.prototype.addLandmark = function(landmark) {
-  this.landmarks.push(landmark);
+Destination.prototype.addLandmark = function (landmark) {
+    this.landmarks.push(landmark);
 }
 
 place1.addLandmark("Stone Hedges");
@@ -16,3 +16,17 @@ place1.addLandmark("Unicorn Island");
 
 console.log(place1.landmarks);
 console.log(place1);
+
+$(document).ready(function () {
+    console.log("JQuery loaded properly.")
+    $("form").submit(function (event) {
+        event.preventDefault();
+        const location = $("#location").val();
+        const landmarks = $("#landmarks").val();
+        const timeOfYear = $("#toy").val();
+        console.log(location);
+        console.log(landmarks);
+        console.log(timeOfYear);
+        console.log("Form submitted.")
+    });
+});
